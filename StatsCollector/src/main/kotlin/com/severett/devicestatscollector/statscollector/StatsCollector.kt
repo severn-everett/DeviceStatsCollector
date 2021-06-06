00fb42cac4a5417bed8cf180f6f4a95d9c3d6bc8
Severn Everett <severn.everett@gmail.com>
2021-06-06 19:18:56 +0200

    Launching CollectorService as a traditional component bean instead of through an implementation of CommandLineRunner

--- StatsCollector/src/main/kotlin/com/severett/devicestatscollector/statscollector/StatsCollector.kt
+++ StatsCollector/src/main/kotlin/com/severett/devicestatscollector/statscollector/StatsCollector.kt
@@ -1,21 +1,10 @@
 package com.severett.devicestatscollector.statscollector
 
-import com.severett.devicestatscollector.statscollector.service.CollectorService
-import mu.KotlinLogging
-import org.springframework.boot.CommandLineRunner
 import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 @SpringBootApplication
-open class StatsCollector(private val collectorService: CollectorService) : CommandLineRunner {
-    private val logger = KotlinLogging.logger { }
-
-    override fun run(vararg args: String) {
-        logger.info { "Starting application" }
-        collectorService.run()
-        logger.info { "Shutting down..." }
-    }
-}
+open class StatsCollector
 
 fun main(args: Array<String>) {
     SpringApplication.run(StatsCollector::class.java, *args)
--- StatsCollector/src/main/kotlin/com/severett/devicestatscollector/statscollector/service/CollectorService.kt
+++ StatsCollector/src/main/kotlin/com/severett/devicestatscollector/statscollector/service/CollectorService.kt
@@ -5,11 +5,13 @@ import mu.KotlinLogging
 import org.eclipse.paho.client.mqttv3.MqttClient
 import org.eclipse.paho.client.mqttv3.MqttConnectOptions
 import org.eclipse.paho.client.mqttv3.persist.MemoryPersistence
-import org.springframework.stereotype.Service
+import org.springframework.stereotype.Component
 import java.util.concurrent.CountDownLatch
+import javax.annotation.PostConstruct
 import javax.annotation.PreDestroy
+import kotlin.concurrent.thread
 
-@Service
+@Component
 class CollectorService(
     private val rabbitMQConfig: RabbitMQConfig,
     private val messageProcessingService: MessageProcessingService
@@ -17,25 +19,28 @@ class CollectorService(
     private val logger = KotlinLogging.logger { }
     private val runningLatch = CountDownLatch(1)
 
+    @PostConstruct
     fun run() {
-        try {
-            logger.info { "Connecting to ${rabbitMQConfig.url} as user ${rabbitMQConfig.userName}" }
-            MqttClient(rabbitMQConfig.url, "StatsCollector", MemoryPersistence()).use { mqttClient ->
-                try {
-                    val connectOptions = MqttConnectOptions().apply {
-                        isAutomaticReconnect = true
-                        userName = rabbitMQConfig.userName
-                        password = rabbitMQConfig.password.toCharArray()
+        thread(name = "CollectorRunner") {
+            try {
+                logger.info { "Connecting to ${rabbitMQConfig.url} as user ${rabbitMQConfig.userName}" }
+                MqttClient(rabbitMQConfig.url, "StatsCollector", MemoryPersistence()).use { mqttClient ->
+                    try {
+                        val connectOptions = MqttConnectOptions().apply {
+                            isAutomaticReconnect = true
+                            userName = rabbitMQConfig.userName
+                            password = rabbitMQConfig.password.toCharArray()
+                        }
+                        mqttClient.connect(connectOptions)
+                        mqttClient.subscribe(rabbitMQConfig.topic, messageProcessingService)
+                        runningLatch.await()
+                    } finally {
+                        mqttClient.disconnect()
                     }
-                    mqttClient.connect(connectOptions)
-                    mqttClient.subscribe(rabbitMQConfig.topic, messageProcessingService)
-                    runningLatch.await()
-                } finally {
-                    mqttClient.disconnect()
                 }
+            } catch (e: Exception) {
+                logger.error(e) { "Unexpected error while running StatsCollector" }
             }
-        } catch (e: Exception) {
-            logger.error(e) { "Unexpected error while running StatsCollector" }
         }
     }
 
